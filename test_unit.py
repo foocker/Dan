5051f4b220418b070fa775334ad158c075b4da60
fq <fengqian1991@gmail.com>
2020-10-30 12:27:52 +0800

    v0.1

--- test_unit.py
+++ test_unit.py
@@ -1,5 +1,7 @@
 from dan.design import Config, Registry
+from dan.detection.detectors import RetinaDet
 from dan.detection.builder import build_plugin, build_backbone, build_detector
+# from dan.detection.netblocks import FPN
 
 
 
@@ -11,13 +13,13 @@ cfg = Config.fromfile("dan/detection/config/face_config.py")
 # print(cfg.train_cfg.cfg_detct.scales, "\n", cfg.train_cfg.xx.save_weights)
 # print(cfg.test_cfg)
 
-# det = build_detector(cfg.model)
+det = build_detector(cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
 
-# plugin = build_plugin(cfg.model)
+# plugin = build_plugin(cfg.model.plugin)
 
-# backb = build_backbone(cfg.model)
+# backb = build_backbone(cfg.model.neck)
 
-# print(det)
+print(det)
 # print(plugin)
 # print(backb)
 
