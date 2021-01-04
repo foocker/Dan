42891bdabd58407677bc50c39fd3543ede22713c
fq <fengqian1991@gmail.com>
2021-01-04 18:53:18 +0800

    semiauto

---
+++ dan/data/autolabel/fake.py
@@ -0,0 +1,16 @@
+def preprocess_image(img):
+    pass
+
+
+class models():
+    def __init__(self):
+        pass
+    
+    def load_model(self, model_path, backbone_name='resnet50'):
+        pass
+    
+    def predict_on_batch(self, x):
+        pass
+    
+    
+models = models()
